59fb76faeb4cb9afee823c2f948e2a3bba947792
Avgurin <58801442+Avgurin@users.noreply.github.com>
2020-01-11 01:46:46 +1100

    1.46

--- binsearch/script.js
+++ binsearch/script.js
@@ -2,20 +2,36 @@ const result = document.getElementById("current-result");
 const btnLess = document.getElementById("btn_less");
 const btnMore = document.getElementById("btn_more");
 
+const maxNum = parseInt(prompt("Загадайте число от 0 до ","100"));
+let currentNum = maxNum-Math.round(maxNum / 2);
+let jump = currentNum;
+let cheat = 0;
+
+showResult(currentNum);
+
+function play(direction) {
+    jump=Math.round(jump / 2);
+    console.log(`cheat= ${cheat} currentNum=${currentNum} jump=${jump}`);
+    if (jump === 1){cheat++};
+    if (cheat === 2 || currentNum+jump > maxNum || currentNum-jump < 0){
+        alert("Не может быть )");
+    } else {
+        currentNum = currentNum + direction * jump;
+        showResult(currentNum);
+    }
+}
+
+function doLess (){
+    play(-1);
+}
+
+function doMore (){
+    play(1);
+}
+
 function showResult(tryNum) {
     result.textContent = tryNum;
 }
 
-
 btnLess.addEventListener ('click',doLess);
 btnMore.addEventListener ('click',doMore);
-
-const maxNum = parseInt(prompt("Загадайте число от 0 до ","50"));
-
-function doLess (){
-    showResult(maxNum);
-}
-
-function doMore (){
-    showResult(maxNum);
-}
