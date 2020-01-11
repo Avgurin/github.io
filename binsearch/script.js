aa4aeac2ce578033cfe3184c0df3d5c97812581a
Avgurin <58801442+Avgurin@users.noreply.github.com>
2020-01-11 11:18:05 +1100

    Update script.js

--- binsearch/script.js
+++ binsearch/script.js
@@ -2,7 +2,34 @@ const result = document.getElementById("current-result");
 const btnLess = document.getElementById("btn_less");
 const btnMore = document.getElementById("btn_more");
 
-const maxNum = parseInt(prompt("Загадайте число от 0 до ","100"));
+let maxNum = parseInt(prompt("Загадайте число от 1 до ","100"));
+let cheat = 0;
+
+if (isNaN(maxNum) || maxNum < 1) {
+    alert("Какую-то фигню ввели. Макс число будет 100");
+    maxNum = 100;
+}
+
+let currentNum = maxNum;
+let jump = Math.round(currentNum / 2);
+play (-1);
+
+function play(direction) { 
+    if (cheat == 2 || 
+        jump<10 && currentNum-jump < 0 ||
+        jump<10 && currentNum+jump > 100){
+        alert("Не может быть )");
+    } else {
+        currentNum = currentNum + direction * jump;
+        showResult(currentNum);
+        jump=Math.round(jump / 2);
+        if (jump === 1){cheat++};
+        console.log(`cheat= ${cheat} currentNum=${currentNum} jump=${jump}`);
+    }
+}
+
+
+/*
 let currentNum = maxNum-Math.round(maxNum / 2);
 let jump = currentNum;
 let cheat = 0;
@@ -20,6 +47,7 @@ function play(direction) {
         showResult(currentNum);
     }
 }
+*/
 
 function doLess (){
     play(-1);
